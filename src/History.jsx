fc1a60d7e49da6399ad5e65dab244d05e9c8b11f
Brenda <brenda@brenda.com>
2019-06-04 11:17:45 +0200

    ....

---
+++ src/History.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class History extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Our History</h1>
+            </div>
+        )
+    }
+}
